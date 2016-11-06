8c32a3fb3e9e733b291039b3a74a7d3c131ffe6c
Eymauzy ALRICK <ealrick@e2r9p2.42.fr>
2016-11-06 18:48:22 +0100

    ajout de ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ealrick <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/06 18:41:28 by ealrick           #+#    #+#             */
+/*   Updated: 2016/11/06 18:47:58 by ealrick          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	int i;
+	int j;
+
+	i = 0;
+	if (little[0] == '\0')
+		return (big);
+	while (i < len)
+	{
+		j = 0;
+		while (little[j] == big[i + j])
+		{
+			if (little[j + 1] == '\0')
+				return (big + i);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
