579470a67d93d796a374db13eae88807e1d23aed
Eymauzy ALRICK <ealrick@e1r3p3.42.fr>
2016-11-10 21:12:36 +0100

    push du 10/11 soir

--- libft.h
+++ libft.h
@@ -6,14 +6,14 @@
 /*   By: ealrick <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 10:25:57 by ealrick           #+#    #+#             */
-/*   Updated: 2016/11/10 16:30:45 by ealrick          ###   ########.fr       */
+/*   Updated: 2016/11/10 21:11:19 by ealrick          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <unistd>
+# include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
 
@@ -24,6 +24,7 @@ int		ft_isalnum(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_isprint(int c);
+void	ft_putnbr(int n);
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 int		ft_strcmp(const char *s1, const char *s2);
@@ -36,8 +37,8 @@ char	*ft_strstr(const char *big, const char *little);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-void	*memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
