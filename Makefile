000b3460c5ef8fabb4e472588deae72c95a3e3eb
Eymauzy ALRICK <ealrick@e3r11p3.42.fr>
2016-11-11 17:39:18 +0100

    push du 11/11 soir

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: ealrick <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/11/10 16:37:00 by ealrick           #+#    #+#              #
-#    Updated: 2016/11/11 15:11:54 by ealrick          ###   ########.fr        #
+#    Updated: 2016/11/11 16:04:09 by ealrick          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,7 +18,7 @@ SRC = ft_isalpha.c ft_isdigit.c ft_isascii.c ft_isalnum.c ft_tolower.c \
 	  ft_strlen.c ft_strstr.c ft_strnstr.c ft_memset.c ft_bzero.c \
 	  ft_memcpy.c ft_memccpy.c ft_memmove.c ft_memchr.c ft_memcmp.c \
 	  ft_putchar_fd.c ft_putstr_fd.c ft_putnbr_fd.c ft_putendl.c \
-	  ft_putendl_fd.c 
+	  ft_putendl_fd.c ft_strcat.c ft_strncat.c
 
 OBJ = $(SRC:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ealrick <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 10:25:57 by ealrick           #+#    #+#             */
-/*   Updated: 2016/11/11 15:07:20 by ealrick          ###   ########.fr       */
+/*   Updated: 2016/11/11 16:03:57 by ealrick          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strdup(char *src);
+char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strncat(char *s1, const char *s2, size_t n);
 int		ft_strlen(const char *str);
 char	*ft_strstr(char *big, char *little);
 char	*ft_strnstr(char *big, char *little, size_t len);
